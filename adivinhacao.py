934e2a91cd2e30e0b4ede6fe2ac432a0652ec2ee
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-08-31 01:07:12 -0300

    Criação do jogo de adivinhação

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,14 @@
+print("********************************");
 print("Bem vindo no jogo de adivinhação");
+print("********************************");
+
+numero_secreto = 42;
+
+chute = input("Digite o seu número: ");
+
+print("Você digitou: ", chute);
+
+if(numero_secreto == int(chute)):
+    print("Você acertou");
+else:
+    print("Você errou");
