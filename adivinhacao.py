f7b9d349c658b088fb2ae2e20ad6f3d7757f9fc9
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-09-04 00:54:01 -0300

    Inclusão de elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,7 +8,16 @@ chute = input("Digite o seu número: ");
 
 print("Você digitou: ", chute);
 
-if(numero_secreto == int(chute)):
+acertou = numero_secreto == int(chute);
+maior = int(chute) > numero_secreto;
+menor = int(chute) < numero_secreto;
+
+if(acertou):
     print("Você acertou");
 else:
-    print("Você errou");
+    if(maior):
+        print("Você errou! O seu chute foi maior que o número secreto.");
+    elif(menor):
+        print("Você errou! O seu número foi menor do que o número secreto");
+
+print("Fim de jogo");
