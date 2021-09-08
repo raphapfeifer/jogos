e6b8a9eb781a1ea34cee23c517ecf03068a64d56
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-09-07 22:15:50 -0300

    Inclusão de while

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,23 +1,29 @@
-print("********************************");
-print("Bem vindo no jogo de adivinhação");
-print("********************************");
+print("********************************")
+print("Bem vindo no jogo de adivinhação")
+print("********************************")
 
-numero_secreto = 42;
+numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu número: ");
+while(rodada <= total_tentativas):
+    print("Tentativa", rodada, "de", total_tentativas)
+    chute = input("Digite o seu número: ")
 
-print("Você digitou: ", chute);
+    print("Você digitou: ", chute)
 
-acertou = numero_secreto == int(chute);
-maior = int(chute) > numero_secreto;
-menor = int(chute) < numero_secreto;
+    acertou = numero_secreto == int(chute)
+    maior = int(chute) > numero_secreto
+    menor = int(chute) < numero_secreto
 
-if(acertou):
-    print("Você acertou");
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.");
-    elif(menor):
-        print("Você errou! O seu número foi menor do que o número secreto");
+    if(acertou):
+        print("Você acertou")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu número foi menor do que o número secreto")
 
-print("Fim de jogo");
+    rodada = rodada +1
+
+print("Fim de jogo")
