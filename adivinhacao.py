a6cb7ce3ad0a6dd2c7b914eb9f0f807ec19ee0e2
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-09-14 23:46:34 -0300

    continue,break e laços

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,11 +6,15 @@ numero_secreto = 42
 total_tentativas = 3
 rodada = 1
 
-while(rodada <= total_tentativas):
-    print("Tentativa", rodada, "de", total_tentativas)
-    chute = input("Digite o seu número: ")
+for rodada in range(1, total_tentativas + 1) :
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    chute = input("Digite um número entre 1 e 100: ")
 
     print("Você digitou: ", chute)
+
+    if (int(chute) < 1 or int(chute) > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = numero_secreto == int(chute)
     maior = int(chute) > numero_secreto
@@ -18,12 +22,12 @@ while(rodada <= total_tentativas):
 
     if(acertou):
         print("Você acertou")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! O seu número foi menor do que o número secreto")
 
-    rodada = rodada +1
 
 print("Fim de jogo")
