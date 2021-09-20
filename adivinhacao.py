edec44c366552a21a2d2a41474ee126a8e0785a3
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-09-19 23:21:44 -0300

    definição de níveis para o jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,20 @@ print("Bem vindo no jogo de adivinhação")
 print("********************************")
 
 numero_secreto = random.randrange(1,101)
-total_tentativas = 3
-rodada = 1
+total_tentativas = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
+
 
 for rodada in range(1, total_tentativas + 1) :
     print("Tentativa {} de {}".format(rodada, total_tentativas))
