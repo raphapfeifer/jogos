314c5bb03089afad9f20ab100d02a631d5efd48b
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-10-02 00:55:48 -0300

    Tratamento para deixar o caracter minusculo e sem espaços

--- forca.py
+++ forca.py
@@ -10,8 +10,16 @@ def jogar_forca():
     acertou = False
 
     while(not enforcou and not acertou):
-        print("jogando...")
 
+        chute = input("Qual letra?").lower().strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index + 1
+
+        print("jogando...")
 
     print("Fim de jogo")
 
