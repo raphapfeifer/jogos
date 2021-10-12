5813f1226ce91210448f866a87644d0d58eb21ec
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-10-12 01:07:25 -0300

    Inclusão de limite de erros e finalização do jogo

--- forca.py
+++ forca.py
@@ -5,22 +5,37 @@ def jogar_forca():
     print("********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra?").lower().strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrei a letra {} na posição {}".format(letra,index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
         print("jogando...")
 
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim de jogo")
 
 if(__name__== "__main__"):
