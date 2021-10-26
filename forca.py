5d2c1dfbc4674a3bea05dd243950701ac93fef9f
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-10-26 00:05:26 -0300

    alteração do arqivo palavras e inclusaõ de lógica de leitura de arquivo

--- forca.py
+++ forca.py
@@ -1,10 +1,22 @@
+import random
 
 def jogar_forca():
     print("********************************")
     print("Bem vindo no jogo de Forca")
     print("********************************")
 
-    palavra_secreta = "maça"
+    arquivo = open("palavra.txt","r")
+    palavras =[]
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+
+    palavra_secreta = palavras[numero]
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
